fe93cc9840a00202b07ce3fb8cd3e2984260b5d8
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-06-22 09:19:26 +0100

    first commit

---
+++ src/app/data.tsx
@@ -0,0 +1,19 @@
+interface Data {
+    city: string;
+    state: string;
+    brand: string;
+    domain: string;
+    logo: string;
+    src: string;
+}
+const data:Data ={
+    brand:"HHS",
+    city:"Houston",
+    state:"TX",
+    domain: "houstonherniaspecialists.com",
+    logo: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1719044205/logo_1_mwb5rw.png',
+    src:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d443088.87942003453!2d-95.40129145!3d29.81717795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640b8b4488d8501%3A0xca0d02def365053b!2sHouston%2C%20TX%2C%20USA!5e0!3m2!1sen!2sng!4v1719044042687!5m2!1sen!2sng"
+
+}
+
+export default data
